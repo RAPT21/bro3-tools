b9d7762ebf3086802f45ca95f42e461dbc4de44c
RAPT21 <admin@rapt21.com>
2019-04-10 01:30:23 +0900

    2017.03.18 同盟盟主座標,同盟貢献ポイントの検出対応、同盟ポイント,同盟貢献ポイントのクエクリ情報対応

--- bro3_initial_quest_helper/bro3_initial_quest_helper.user.js
+++ bro3_initial_quest_helper/bro3_initial_quest_helper.user.js
@@ -4,17 +4,18 @@
 // @description	ブラウザ三国志 初期クエクリ補助 by RAPT
 // @include		http://*.3gokushi.jp/quest*
 // @exclude		http://*.3gokushi.jp/maintenance*
-// @require		http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
+// @require		http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js
 // @connect		3gokushi.jp
 // @grant		GM_xmlhttpRequest
 // @author		RAPT
-// @version 	2016.08.17
+// @version 	2017.03.18
 // ==/UserScript==
-var VERSION = "2016.08.17"; 	// バージョン情報
+var VERSION = "2017.03.18"; 	// バージョン情報
 
 
 // 2016.07.25 初版作成
 // 2016.08.17 Firefox サポート
+// 2017.03.18 同盟盟主座標,同盟貢献ポイントの検出対応、同盟ポイント,同盟貢献ポイントのクエクリ情報対応
 
 
 jQuery.noConflict();
@@ -60,27 +61,141 @@ var $a = function(xp,dc) { var r = d.evaluate(xp, dc||d, null, XPathResult.ORDER
 var $e = function(e,t,f) { if (!e) return; e.addEventListener(t, f, false); };
 var $v = function(key) { return d.evaluate(key, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null); };		// 2013.12.18
 
+
+
+
+
+//------------------------------------------------------------
+//	▼▼▼2017.01.xx▼▼▼
+//------------------------------------------------------------
+
+// 基本
+var ID_ALLY_RECRUIT	= 249; // 同盟募集機能を使ってみよう
+var ID_TOTAL_RANK	= 123; // 統計の総合ランキングの順位を確認する
+var ID_ALLY_RANK	= 106; // 現在の同盟ランクを報告する
+var ID_WEEKLY_RANK	= 166; // 週間ランキングを確認しよう
+
+// 同盟
+var ID_ALLY_CONTR_1	= 210; // 同盟貢献ポイントを1000以上にする
+var ID_ALLY_CONTR_2	= 211; // 同盟貢献ポイントを3000以上にする
+var ID_ALLY_CONTR_3	= 212; // 同盟貢献ポイントを6000以上にする
+var ID_ALLY_CONTR_4	= 213; // 同盟貢献ポイントを10000以上にする
+var ID_ALLY_CONTR_5	= 214; // 同盟貢献ポイントを30000以上にする
+var ID_ALLY_CONTR_6	= 215; // 同盟貢献ポイントを60000以上にする
+var ID_ALLY_CONTR_7	= 216; // 同盟貢献ポイントを100000以上にする
+var ID_ALLY_CONTR_8	= 217; // 同盟貢献ポイントを300000以上にする
+
+var ID_ALLY_POINT_1	= 220; // 同盟ポイントを40000以上にする
+var ID_ALLY_POINT_2	= 221; // 同盟ポイントを80000以上にする
+var ID_ALLY_POINT_3	= 222; // 同盟ポイントを160000以上にする
+var ID_ALLY_POINT_4	= 223; // 同盟ポイントを400000以上にする
+var ID_ALLY_POINT_5	= 224; // 同盟ポイントを800000以上にする
+var ID_ALLY_POINT_6	= 225; // 同盟ポイントを1600000以上にする
+var ID_ALLY_POINT_7	= 226; // 同盟ポイントを4000000以上にする
+var ID_ALLY_POINT_8	= 227; // 同盟ポイントを8000000以上にする
+
+// 同盟貢献ポイント達成確認
+function check_ally_contribute(pt, qid){
+	if (qid == ID_ALLY_CONTR_1 && pt >= 1000) return true;
+	if (qid == ID_ALLY_CONTR_2 && pt >= 3000) return true;
+	if (qid == ID_ALLY_CONTR_3 && pt >= 6000) return true;
+	if (qid == ID_ALLY_CONTR_4 && pt >= 10000) return true;
+	if (qid == ID_ALLY_CONTR_5 && pt >= 30000) return true;
+	if (qid == ID_ALLY_CONTR_6 && pt >= 60000) return true;
+	if (qid == ID_ALLY_CONTR_7 && pt >= 100000) return true;
+	if (qid == ID_ALLY_CONTR_8 && pt >= 300000) return true;
+	return false;
+}
+
+// 同盟ポイント達成確認
+function check_ally_point(pt, qid){
+	if (qid == ID_ALLY_POINT_1 && pt >= 40000) return true;
+	if (qid == ID_ALLY_POINT_2 && pt >= 80000) return true;
+	if (qid == ID_ALLY_POINT_3 && pt >= 160000) return true;
+	if (qid == ID_ALLY_POINT_4 && pt >= 400000) return true;
+	if (qid == ID_ALLY_POINT_5 && pt >= 800000) return true;
+	if (qid == ID_ALLY_POINT_6 && pt >= 1600000) return true;
+	if (qid == ID_ALLY_POINT_7 && pt >= 4000000) return true;
+	if (qid == ID_ALLY_POINT_8 && pt >= 8000000) return true;
+	return false;
+}
+
+function debug_ally_contribute(pt) {
+	var key = "　→同盟に貢献しよう"
+	var reach = ""
+	if (pt >= 1000) reach = "其一";
+	if (pt >= 3000) reach = "其二";
+	if (pt >= 6000) reach = "其三";
+	if (pt >= 10000) reach = "其四";
+	if (pt >= 30000) reach = "其五";
+	if (pt >= 60000) reach = "其六";
+	if (pt >= 100000) reach = "其七";
+	if (pt >= 300000) reach = "其八";
+	if (reach.length > 0) {
+		appendLog(key, reach + "までクリア！");
+	} else {
+		appendLog(key, "未達");
+	}
+}
+function debug_ally_point(pt) {
+	var key = "　→同盟上位への道"
+	var reach = ""
+	if (pt >= 40000) reach = "其一";
+	if (pt >= 80000) reach = "其二";
+	if (pt >= 160000) reach = "其三";
+	if (pt >= 400000) reach = "其四";
+	if (pt >= 800000) reach = "其五";
+	if (pt >= 1600000) reach = "其六";
+	if (pt >= 4000000) reach = "其七";
+	if (pt >= 8000000) reach = "其八";
+	if (reach.length > 0) {
+		appendLog(key, reach + "までクリア！");
+	} else {
+		appendLog(key, "未達");
+	}
+}
+
+
+//------------------------------------------------------------
+//	▼▼▼2016.08.17▼▼▼
+//------------------------------------------------------------
+function appendLog(key, value) {
+	console.log(key + ':' + value);
+}
+
 // 個人ランク
 function self_rank(){
 	httpGET('http://'+HOST+'/user/ranking.php',function(x){
 		var htmldoc = document.createElement("html");
 			htmldoc.innerHTML = x;
 		var rankNum = j$('.mydata>.rankNum', htmldoc).text().substr(1).trim();
-		console.log("個人ランク:"+rankNum);
+		appendLog("個人ランク", rankNum);
 	});
 }
 
-// 同盟ランク,同盟ポイント,同盟人数
-function ally_rank(){
+// 同盟ランク,同盟ポイント,同盟人数,同盟盟主座標
+function ally_rank(fn){
 	httpGET('http://'+HOST+'/alliance/list.php',function(x){
 		var htmldoc = document.createElement("html");
 			htmldoc.innerHTML = x;
-		var rankNum = j$('.mydata>.rankNum', htmldoc).text().substr(1).trim();
-		console.log("同盟ランク:"+rankNum);
-		var allyPoint = j$('.mydata>td:eq(2)', htmldoc).text().trim();
-		console.log("同盟ポイント:"+allyPoint);
-		var allyMembers = j$('.mydata>td:eq(3)', htmldoc).text().trim();
-		console.log("同盟人数:"+allyMembers);
+		var td = j$('tr.mydata td', htmldoc);
+		var leaderXY = td.eq(5).text().trim();
+		appendLog("同盟盟主座標", leaderXY);
+		var rankNum = td.eq(0).text().substr(1).trim();
+		appendLog("同盟ランク", rankNum);
+		var allyMembers = td.eq(3).text().trim();
+		appendLog("同盟人数", allyMembers);
+		var allyPoint = td.eq(2).text().trim();
+		appendLog("同盟ポイント", allyPoint);
+		debug_ally_point(allyPoint);
+
+		var allyUrl = j$("a", td.eq(1)).attr("href");
+		if (allyUrl.indexOf("info.php?id=") == 0) {
+			allyUrl = 'http://'+HOST+'/alliance/' + allyUrl;
+		}
+		if (fn !== null) {
+			fn(allyUrl);
+		}
 	});
 }
 
@@ -91,7 +206,7 @@ function weekly_rank(){
 			htmldoc.innerHTML = x;
 		var base = j$('.tables[summary="攻撃ランキング"]', htmldoc);
 		var rankNum = j$('.rank-self>td:first', base).text().substr(1).trim();
-		console.log("週間ランキング:"+rankNum);
+		appendLog("週間ランキング", rankNum);
 	});
 }
 
@@ -104,9 +219,9 @@ function profile_jinko(fn){
 		var name = text.match(/お気に入り武将カード君主(.+)個人掲示板/)[1];
 		var sumJinko = text.match(/総人口(\d+)/)[1];
 		var attScore = text.match(/撃破スコア(\d+)/)[1];
-		console.log("君主:"+name);
-		console.log("総人口:"+sumJinko);
-		console.log("撃破スコア:"+attScore);
+		appendLog("君主", name);
+		appendLog("総人口", sumJinko);
+		appendLog("撃破スコア", attScore);
 		if (fn !== null) {
 			fn(name);
 		}
@@ -114,18 +229,22 @@ function profile_jinko(fn){
 }
 
 // 同盟貢献ポイント
-function ally_info(name){
-	httpGET('http://'+HOST+'/alliance/info.php?id=2896',function(x){
+function ally_info(url, name){
+	httpGET(url,function(x){
 		var htmldoc = document.createElement("html");
 			htmldoc.innerHTML = x;
-		var table = j$('table.tables[summary="ランキング"]', htmldoc);
-		var tr = j$('tr', table);
+
+		var tr = j$("table[class='tables'] tbody tr", htmldoc);
 		for (var i = 0; i < tr.length; ++i) {
-			var aName = j$('td:eq(1)', tr[i]).text().replace(/\s+/g, '');
-			var aScore = j$('td:eq(2)', tr[i]).text().replace(/\s+/g, '');
-			if (aName == name) {
-				console.log("同盟貢献ポイント:"+aScore);
-				break;
+			var td = j$("td", tr.eq(i));
+			if (td.length > 0) {
+				var aName = td.eq(1).text().replace(/\s+/g, '');
+				var aScore = td.eq(2).text().replace(/\s+/g, '');
+				if (aName == name) {
+					appendLog("同盟貢献ポイント", aScore);
+					debug_ally_contribute(aScore);
+					break;
+				}
 			}
 		}
 	});
@@ -133,7 +252,10 @@ function ally_info(name){
 
 ( function() {
 	self_rank();
-	ally_rank();
 	weekly_rank();
-	profile_jinko(ally_info);
+	profile_jinko(function(username){
+	ally_rank(function(ally_url){
+			ally_info(ally_url, username);
+		});
+	});
 })();
