6a6ed4923f5b898c2b78e6efabf478c6cf206a90
pla2999 <pla@246.ne.jp>
2024-11-22 12:07:00 +0900

    本拠地＋のデッキコストに空きがある場合でも、本拠に内政官が設定されている場合には城壁塔に内政官を１クリックで設定できない問題の修正

--- bro3_beyond/bro3_beyond.user.js
+++ bro3_beyond/bro3_beyond.user.js
@@ -7901,13 +7901,16 @@ function addSkillViewOnSmallCardDeck(is_draw_passive, is_draw_use_link, is_draw_
 		// 回復スキルは最初に見つかった通常拠点で発動させればよい。空きコスト大きいほうでよい
 		var mainVacantId = 0;
 		var subVacantId = 0;
+		var isMainFound = false;
+		var isSubFound = false;
 		q$.each(villages, function() {
 			if (this.isset_domestic === false) { // 内政官不在
-				if (villages[0] === this) {
+				if (isMainFound === false && this.deck_kind === 1) {
 					mainVacantId = this.village_id;
-				} else {
+					isMainFound = true;
+				} else if (isSubFound === false) {
 					subVacantId = this.village_id;
-					return false;
+					isSubFound = true;
 				}
 			}
 		});
@@ -8100,7 +8103,7 @@ function addSkillViewOnSmallCardDeck(is_draw_passive, is_draw_use_link, is_draw_
 										q$(this).html("<span class='skb'>[使用]</span>");
 										return;
 									}
-									var vacant_cost = (villages[0].village_id === parseInt(village_id, 10) || village_info.village_name.slice(0,3) === "城壁塔") ? domesticMainVacantCost : domesticSubVacantCost;
+									var vacant_cost = (village_info.deck_kind === 1) ? domesticMainVacantCost : domesticSubVacantCost;
 									if (card_cost > vacant_cost) {
 										alert(`${village_info.village_name}の空きコストが不足しています`);
 										q$(this).parent().children('td').html(recover_html);
