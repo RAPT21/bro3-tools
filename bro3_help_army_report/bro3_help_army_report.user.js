416887a90ce920e0df4717dc14c9339d303facb2
RAPT21 <admin@rapt21.com>
2025-09-25 23:21:10 +0900

    [help_army_report] 南蛮援軍確認ツールで同盟ログ対応

--- bro3_help_army_report/bro3_help_army_report.user.js
+++ bro3_help_army_report/bro3_help_army_report.user.js
@@ -6,21 +6,24 @@
 // @include		https://*.3gokushi.jp/report/detail.php*
 // @include		http://*.3gokushi.jp/facility/unit_status.php?type=all*
 // @include		https://*.3gokushi.jp/facility/unit_status.php?type=all*
+// @include		http://*.3gokushi.jp/alliance/detail.php*
+// @include		https://*.3gokushi.jp/alliance/detail.php*
 // @exclude		http://*.3gokushi.jp/maintenance*
 // @exclude		https://*.3gokushi.jp/maintenance*
 // @require		https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @connect		3gokushi.jp
 // @grant		none
 // @author		RAPT
-// @version 	1.0
+// @version 	1.1
 // ==/UserScript==
 jQuery.noConflict();
 q$ = jQuery;
 
 // 2023/02/12	1.0	初版作成
+// 2025/09/25	1.1	同盟ログ対応
 
 
-var OPT_MIN_SOLDIER_COUNT = 40000; // 援軍兵士として許容する最小兵数
+var OPT_MIN_SOLDIER_COUNT = 60000; // 援軍兵士として許容する最小兵数
 
 // インデックスを合わせて合成
 function zip(keys, values) {
@@ -39,6 +42,16 @@ function parseReport() {
 	var warnUsers = new Set();
 	var warnReasons = {};
 	var villages = new Set();
+
+	// 南蛮主催者を取得
+	var text = q$("#gray02Wrapper table[class*='tables'][summary='件名'] tr td.brno:contains('が南蛮兵に攻撃されました')").text();
+	var match = text.match(/【.*?】.*?【.*?】(.*?)　\(.*?\)　が南蛮兵に攻撃されました/);
+	if (match === null || !match.length) {
+		return "";
+	}
+	var organizer = match[1];
+
+	// 援軍を解析
 	q$("#gray02Wrapper table[class*='tables'][summary='防御者'] tbody").each(function(index, row){
 		var defenser = q$("tr th[class='defenser']", row).text();
 		if (defenser.indexOf('援軍') < 0) {
@@ -100,7 +113,8 @@ function parseReport() {
 		var high = sols['矛槍兵'] + sols['弩兵'] + sols['近衛騎兵'];
 		var ei = sols['戦斧兵'] + sols['双剣兵'] + sols['大錘兵'];
 		var busho = sols['武将'];
-		var others = totalCount - ken - tate - high - ei - busho;
+		// var others = totalCount - ken - tate - high - ei - busho;
+		var others = totalCount - tate - ei - busho;
 
 		var warn = [];
 		if (villages.has(vName)) {
@@ -110,13 +124,14 @@ function parseReport() {
 			warn.push("1つの拠点から複数武将");
 		}
 		if (others > 0) {
-			warn.push("大剣、重盾、上級、鋭兵以外の兵士がいる");
+			// warn.push("大剣、重盾、上級、鋭兵以外の兵士がいる");
+			warn.push("重盾、鋭兵以外の兵士がいる");
 		}
-		if (totalCount === 0) {
-			warn.push("援軍兵0");
+		if ((totalCount - busho) < OPT_MIN_SOLDIER_COUNT) {
+			warn.push("援軍兵不足");
 		}
 
-		if (warn.length) {
+		if (warn.length && userName !== organizer) {
 			warnUsers.add(userName);
 			warnReasons[userName] = warn;
 		}
@@ -125,11 +140,15 @@ function parseReport() {
 		villages.add(vName);
 
 		if (list.length === 0) {
-			list.push(['武将', '大剣', '重盾', '上級', '鋭兵', '他兵', '君主名', '拠点名'].join("\t"));
+			// list.push(['武将', '大剣', '重盾', '上級', '鋭兵', '他兵', '君主名', '拠点名'].join("\t"));
+			list.push(['武将', '重盾', '鋭兵', '他兵', '君主名', '拠点名'].join("\t"));
 		}
-		list.push([busho, ken, tate, high, ei, others, userName, vName].join("\t"));
+		// list.push([busho, ken, tate, high, ei, others, userName, vName].join("\t"));
+		list.push([busho, tate, ei, others, userName, vName].join("\t"));
 	});
+
 	var info = [];
+	info.push(`主催: ${organizer}`);
 	info.push(`援軍部隊数: ${list.length}`);
 	info.push(`援軍君主数: ${users.size}`);
 	info.push(`警告君主数: ${warnUsers.size}`);
@@ -182,7 +201,8 @@ function parseUnit() {
 		var high = sols['矛槍兵'] + sols['弩兵'] + sols['近衛騎兵'];
 		var ei = sols['戦斧兵'] + sols['双剣兵'] + sols['大錘兵'];
 		var busho = sols['武将'];
-		var validCount = ken + tate + high + ei; // 下級兵や斥候、兵器はノーカン
+		// var validCount = ken + tate + high + ei; // 下級兵や斥候、兵器はノーカン
+		var validCount = tate + ei; // 下級兵や斥候、兵器はノーカン
 		var others = totalCount - validCount - busho;
 
 		if ((busho > 0 && (totalCount - busho) >= OPT_MIN_SOLDIER_COUNT) || busho === 0) {
@@ -232,6 +252,7 @@ function parseUnit() {
 		var high = sols['矛槍兵'] + sols['弩兵'] + sols['近衛騎兵'];
 		var ei = sols['戦斧兵'] + sols['双剣兵'] + sols['大錘兵'];
 		var busho = sols['武将'];
+		// var validCount = tate + ei; // 下級兵や斥候、兵器はノーカン
 		var validCount = ken + tate + high + ei; // 下級兵や斥候、兵器はノーカン
 		var others = totalCount - validCount - busho;
 
@@ -283,7 +304,9 @@ q$(function(){
 				click: function(){
 					var result = parseReport();
 					console.log(result);
-					alert(result);
+					if (result.length) {
+						alert(result);
+					}
 				}
 			}
 		}).append("解析"));
@@ -297,7 +320,26 @@ q$(function(){
 				click: function(){
 					var result = parseUnit();
 					console.log(result);
-					alert(result);
+					if (result.length) {
+						alert(result);
+					}
+				}
+			}
+		}).append("解析"));
+	}
+
+	// 同盟ログ
+	var urlParams = new URLSearchParams(location.search);
+	if (location.pathname === '/alliance/detail.php' && urlParams.get('m') === 'defense' && urlParams.has('id')) {
+		q$("#gray02Wrapper table[class*='tables'][summary='件名']").before(q$("<button />", {
+			id: 'parseReport',
+			on: {
+				click: function(){
+					var result = parseReport();
+					console.log(result);
+					if (result.length) {
+						alert(result);
+					}
 				}
 			}
 		}).append("解析"));
